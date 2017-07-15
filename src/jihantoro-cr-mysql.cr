8b1c9d0bb551d47e10c9112558848640d515f240
Rubi Jihantoro <jihantoro8@gmail.com>
2017-07-15 22:20:38 +0700

    main file

--- src/jihantoro-cr-mysql.cr
+++ src/jihantoro-cr-mysql.cr
@@ -21,7 +21,30 @@ db.exec "update friend set name=? where name=?", newfriend, myfriend # update (U
 puts "deleting data..."
 db.exec "delete from friend where name=?", newfriend
 
-puts "Done"
-puts "dont forget to visit http://jihantoro.net"
+puts "We want more..."
+friends = ["Ary","Isacc","Fridgerator","Serdar","BrigeBOT xD"]
+(0...5).each do |i|
+  name = friends[i].to_s
+  db.exec "insert into friend (name) values (?)", name
+  puts "Hi " + name
+end
 
-db.close
+puts "i forgot all of my friend, :O"
+puts "reading memory. . .."
+db.query "select name from friend" do |data|
+  data.each do
+    friend = data.read(String)
+    puts friend + " is your friend"
+  end
+end
+
+one = db.query_one? "select name from friend where name='Ary' limit 1", as:{String}
+if one
+  one + " is still my friend"
+end
+
+db.exec "delete from friend"
+
+puts "Bye..."
+
+puts "but, dont forget to visit http://jihantoro.net"
